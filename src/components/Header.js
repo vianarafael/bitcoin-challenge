cab27fb3d7865f553938c28fb6c15ba5b44920f4
vianarafael <vianarafaelds@gmail.com>
2021-07-16 11:41:23 +0900

    added some header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,45 @@
 import { connect } from "react-redux";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 50px 8px;
+  width: 100%;
+  margin: 1rem auto 3rem;
+
+  div {
+    width: 100%;
+    display: flex;
+    justify-content: left;
+  }
+
+  img {
+    height: 22px;
+  }
+
+  h2 {
+    height: 22px;
+  }
+`;
 
 const Header = ({ currentPrice }) => {
   return (
-    <>
-      <img
-        src="https://menu.cdn.bitcoindotcom.net/uni/dist/assets/images/logo_black.png"
-        alt="Bitcoin.com logo"
-      />
-      <h2>Current Price: ${`${currentPrice}`}</h2>
-    </>
+    <HeaderContainer>
+      <div>
+        <div>
+          <img
+            src="https://menu.cdn.bitcoindotcom.net/uni/dist/assets/images/logo_black.png"
+            alt="Bitcoin.com logo"
+          />
+        </div>
+        <div>
+          <h2>Current BCH Price: ${`${currentPrice}`}</h2>
+        </div>
+      </div>
+    </HeaderContainer>
   );
 };
 
